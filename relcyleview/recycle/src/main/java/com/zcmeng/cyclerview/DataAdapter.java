ce730477f287fd6f6601b4b2e04467ae2d7fe8f1
zcmeng <1042086562@qq.com>
2019-11-05 22:24:12 +0800

    重新提交代码

---
+++ relcyleview/recycle/src/main/java/com/zcmeng/cyclerview/DataAdapter.java
@@ -0,0 +1,103 @@
+package com.zcmeng.cyclerview;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by mr.gao on 2018/1/14.
+ * Package:    mrgao.com.recyclerviewtext.loadMore.adapter
+ * Create Date:2018/1/14
+ * Project Name:RecyclerViewText
+ * Description:
+ */
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ItemHolder> {
+
+    List<String> mStringList;
+
+
+    public DataAdapter() {
+        mStringList = new ArrayList<>();
+    }
+
+    @Override
+    public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, final int position) {
+        if (mStringList.size() != 0) {
+            holder.mTextView.setText(mStringList.get(position));
+        }
+
+
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, int position, List<Object> payloads) {
+         super.onBindViewHolder(holder, position, payloads);
+        if (payloads.isEmpty()) {
+            Log.i("LoadingActivity", "不走局部刷新" + position);
+            onBindViewHolder(holder, position);
+        } else {
+            //这里做一些局部刷新的东西
+
+            Log.i("LoadingActivity", "当前的位置" + position);
+
+            String item = mStringList.get(position);
+            holder.mTextView.setText("我是局部刷新的" + position + "(old=>)" + item);
+
+
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mStringList.size();
+    }
+
+    public void addAll(List<String> list) {
+        mStringList.addAll(list);
+        //全部刷新
+        notifyDataSetChanged();
+    }
+
+    public void clearAll() {
+        mStringList.clear();
+        notifyDataSetChanged();
+    }
+
+
+
+
+    public class ItemHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            mTextView = getView(itemView, R.id.item);
+        }
+    }
+
+    private <T extends View> T getView(View view, int id) {
+        return (T) view.findViewById(id);
+    }
+
+}
---
+++ relcyleview/recycle/src/main/java/com/zcmeng/cyclerview/interfaces/LucklyRecyclerSwpieInterface.java
@@ -0,0 +1,36 @@
+package com.zcmeng.cyclerview.interfaces;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.zcmeng.cyclerview.LucklyRecyclerView;
+
+
+/**
+ * Created by mr.gao on 2018/1/16.
+ * Package:    mrgao.com.recyclerviewtext.loadMore.views
+ * Create Date:2018/1/16
+ * Project Name:RecyclerViewText
+ * Description:
+ */
+
+public interface LucklyRecyclerSwpieInterface {
+    boolean isRefreshing();
+
+    void setColorSchemeResources(int... colorResIds);
+
+    void setColorSchemeColors(int... colors);
+
+    void setRefreshing(boolean refreshing);
+
+    void setSwipeRefreshLayoutEnable(boolean enable);
+
+    SwipeRefreshLayout getSwipeRefreshLayout();
+
+    void setOnRefreshListener(LucklyRecyclerView.OnRefreshListener listener);
+
+    void setProgressViewOffset(boolean scale, int start, int end);
+
+    void setProgressViewEndTarget(boolean scale, int end);
+
+    void setProgressBackgroundColor(int colorRes);
+}
